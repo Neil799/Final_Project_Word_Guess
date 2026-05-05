aa610703e9537a8653672ea06ebfab62cbb114de
Gandhi <neil.gandhi29@bcp.org>
2026-05-05 13:38:54 -0700

    Functional! Base version works, many things still n eeded like backspace win screen, sound effects, word enter confirm stuff, but it is FUNCTIONAL

--- Board.py
+++ Board.py
@@ -1,13 +1,13 @@
-import pygame
-import random
-from Tile import Tile
+# import pygame
+# import random
+# from Tile import Tile
 
 
 
-class Board:
-    print("Board!")
-    def word_chooser(self):
-        words = ["sleep", "drink", "basin"]
-        secret_word = random.randint(words)
-        print(secret_word)
-    # note: this class will store guesses and determine guess correctness
+# class Board:
+#     print("Board!")
+#     def word_chooser(self):
+#         words = ["sleep", "drink", "basin"]
+#         secret_word = random.randint(words)
+#         print(secret_word)
+#     # note: this class will store guesses and determine guess correctness
--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 # imports
 import pygame
+import random
 from pygame import mixer
 from Tile import Tile
-from Board import Board
+# from Board import Board
+import time
 pygame.init()
 
 
@@ -18,6 +20,10 @@ empty_box_color = (194,197,204)
 font = pygame.font.SysFont('Felix Titling', 50)
 
 def main():
+    column = 0
+    feedback = ""
+    words = ["slate", "drink", "jumpy","emcee","snake","cobra", "glass", "trait", "water", "bells", "preps", "pants", "shoes", "grade"]
+    target_word = random.choice(words)
     word = ""
     LETTER_X_POS = 40
     LETTER_Y_POS = 20
@@ -37,16 +43,38 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.unicode.isalpha():
                     letter = event.unicode
-                    print(letter)
                     word += letter
                     letter_surface = font.render(letter, True, FONT_COLOR)
                     screen.blit(letter_surface, (LETTER_X_POS, LETTER_Y_POS))
                     LETTER_X_POS+=85
                     if len(word) == 5:
-                        print("hooray! "+word)
+                        for i in range(5):
+                            if word[i] == target_word[i]:
+                                letter_surface = font.render(word[i], True, "Green")
+                                screen.blit(letter_surface, (40+i*85, 20+column*90))
+                            elif word[i] in target_word:
+                                letter_surface = font.render(word[i], True, "Yellow")
+                                screen.blit(letter_surface, (40+i*85, 20+column*90))
+                            else:
+                                letter_surface = font.render(word[i], True, "Grey")
+                                screen.blit(letter_surface, (40+i*85, 20+column*90))
+                        # note: make it enter so change vvvv
+                        if word == target_word:
+                            print("you won!")
+                            
+                        elif column != 5:
+                            column +=1
+                        else:
+                            print("You lost, word was " + target_word)
+                    # elif event.key == pygame.K_BACKSPACE:
+                        
+                        
+
+                        print(feedback)
                         LETTER_X_POS = 40
                         LETTER_Y_POS += 90
                         word = ""
+                        feedback = ""
 
                     # note add most of the above stuff into board class
             if event.type == pygame.QUIT:
