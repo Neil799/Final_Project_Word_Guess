67d95b266c9332e83f2bf65f4053db2c205bb962
Gandhi <neil.gandhi29@bcp.org>
2026-05-07 11:46:50 -0700

    Got backspace and enter to work better interface essentially

--- Board.py
+++ Board.py
@@ -1,10 +1,23 @@
-# import pygame
+import pygame
 # import random
 # from Tile import Tile
 
 
 
-# class Board:
+class Board:
+
+     def __init__(self, name):
+        self.name = name
+     def determine_correctness(self, letter, answer_letter, answer):
+          if letter == answer_letter:
+            return "Green"
+          elif letter in answer:
+               return "Yellow"
+          else:
+            return "Grey"
+     def redraw(self, screen, letter, color, font, x_pos, y_pos):
+          letter_surface = font.render(letter, True, color)
+          screen.blit(letter_surface, (x_pos, y_pos))
 #     print("Board!")
 #     def word_chooser(self):
 #         words = ["sleep", "drink", "basin"]
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 import random
 from pygame import mixer
 from Tile import Tile
-# from Board import Board
+from Board import Board
 import time
 pygame.init()
 
@@ -28,9 +28,13 @@ def main():
     LETTER_X_POS = 40
     LETTER_Y_POS = 20
     letter = ""
+    colors_list = []
+    line_skip_counter = 0
+    letters_list = []
     running = True
-    
+    done = False
     # box = Tile(14,17,77,77,empty_box_color)
+    wordle_board = Board("Wordle_Board")
     grid = pygame.image.load('blankgrid.jpg').convert_alpha()
     screen.blit(grid, (0, 0))
     while running:
@@ -38,47 +42,69 @@ def main():
         
         # box.display(screen)
         pygame.display.update()
-
+        
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.unicode.isalpha():
-                    letter = event.unicode
-                    word += letter
-                    letter_surface = font.render(letter, True, FONT_COLOR)
-                    screen.blit(letter_surface, (LETTER_X_POS, LETTER_Y_POS))
-                    LETTER_X_POS+=85
-                    if len(word) == 5:
-                        for i in range(5):
-                            if word[i] == target_word[i]:
-                                letter_surface = font.render(word[i], True, "Green")
-                                screen.blit(letter_surface, (40+i*85, 20+column*90))
-                            elif word[i] in target_word:
-                                letter_surface = font.render(word[i], True, "Yellow")
-                                screen.blit(letter_surface, (40+i*85, 20+column*90))
-                            else:
-                                letter_surface = font.render(word[i], True, "Grey")
-                                screen.blit(letter_surface, (40+i*85, 20+column*90))
-                        # note: make it enter so change vvvv
-                        if word == target_word:
-                            print("you won!")
-                            
-                        elif column != 5:
-                            column +=1
-                        else:
-                            print("You lost, word was " + target_word)
-                    # elif event.key == pygame.K_BACKSPACE:
-                        
-                        
-
-                        print(feedback)
-                        LETTER_X_POS = 40
-                        LETTER_Y_POS += 90
-                        word = ""
-                        feedback = ""
-
-                    # note add most of the above stuff into board class
             if event.type == pygame.QUIT:
                 pygame.quit()
+            if done == False:
+                if event.type == pygame.KEYDOWN:
+                    if event.unicode.isalpha():
+                        if len(word) != 5:
+                            letter = event.unicode
+                            word += letter
+                            print(word)
+                            letter_surface = font.render(letter, True, FONT_COLOR)
+                            screen.blit(letter_surface, (LETTER_X_POS, LETTER_Y_POS))
+                            LETTER_X_POS+=85
+                            
+
+                            # elif event.key == pygame.K_BACKSPACE:
+                    
+                    
+                    elif event.key == pygame.K_RETURN:
+                        
+                        if len(word) == 5:
+                                for i in range(5):
+                                    color = wordle_board.determine_correctness(word[i], target_word[i], target_word)
+                                    letter_surface = font.render(word[i], True, color)
+                                    screen.blit(letter_surface, (40+i*85, 20+column*90))
+                                    colors_list.append(color)
+                                    letters_list.append(word[i])
+                                # note: make it enter so change vvvv
+                                if word == target_word:
+                                    print("you won!")
+                                    done = True
+                                elif column != 5:
+                                    column +=1
+                                else:
+                                    print("You lost, word was " + target_word)
+                                    done = True
+                        
+                                print("enter key")
+                                LETTER_X_POS = 40
+                                LETTER_Y_POS += 90
+                                word = ""
+                        # note add most of the above stuff into board class
+                    elif event.key == pygame.K_BACKSPACE:
+                        if len(word) != 0:
+                            word = ""
+                            screen.blit(grid, (0,0))
+                            LETTER_X_POS = 40
+                            LETTER_Y_POS = 20
+                            for j in range(len(letters_list)):
+                                
+                                wordle_board.redraw(screen, letters_list[j], colors_list[j], font, LETTER_X_POS, LETTER_Y_POS)
+                                LETTER_X_POS += 85
+                                line_skip_counter+=1
+                                if line_skip_counter == 5:
+                                    LETTER_Y_POS += 90
+                                    line_skip_counter = 0
+                                    LETTER_X_POS = 40
+                                
+                                
+
+                            
+                
             
 if __name__ == "__main__":
     main()
